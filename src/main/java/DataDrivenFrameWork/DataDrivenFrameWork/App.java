9154e7630353d9ce6950e852eb63157fa3966b12
YashuQA <yaseswiniy@duck.com>
2023-03-04 15:36:02 +0530

    Data Driven FrameWork - Maven Project Created

---
+++ src/main/java/DataDrivenFrameWork/DataDrivenFrameWork/App.java
@@ -0,0 +1,13 @@
+package DataDrivenFrameWork.DataDrivenFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
